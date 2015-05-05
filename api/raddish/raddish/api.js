8bb3c12dfbd368e099d2fce78ba842ab6fca0a08
Jasper van Rijbroek <jasper@jvar.nl>
2015-05-05 11:36:39 +0200

    Updated docs
    Updated styling and documentation. Further improvements are needed.

---
+++ api/raddish/raddish/api.js
@@ -0,0 +1,41 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AbstractAdapter",
+        "AbstractDispatcher",
+        "Application",
+        "CommandBehavior",
+        "CommandChain",
+        "CommandContext",
+        "Controller",
+        "ControllerAbstract",
+        "DefaultAuthenticator",
+        "Dispatcher",
+        "Inflector",
+        "Mixin",
+        "Model",
+        "ModelAbstract",
+        "MongoAdapter",
+        "MysqlAdapter",
+        "ObjectIdentifier",
+        "ObjectLoader",
+        "ObjectManager",
+        "Plugin",
+        "RaddishError",
+        "Role",
+        "Router",
+        "Row",
+        "RowAbstract",
+        "Rowset",
+        "RowsetAbstract",
+        "Socket",
+        "SqliteAdapter",
+        "States",
+        "Table",
+        "ViewAbstract",
+        "ViewJson"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
